03b5296900db2ce39407543a0f421c89d10d0372
Mattias Soroccos <matte_1500@hotmail.com>
2018-04-20 15:46:48 +0200

    Init Commit

---
+++ DotNetSocketLearning/DotNetSocketLearning/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotNetSocketLearning")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetSocketLearning")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("26cec7cb-622e-44a1-80d0-fd10cb8fe3a2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetSocketLearning/MessageExample/Networking/ServerSocket.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MessageExample.Networking
+{
+
+    class ServerSocket
+    {
+        private Socket _socket;
+        byte[] _buffer = new byte[1024];
+
+        public ServerSocket()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Bind(int port)
+        {
+            _socket.Bind(new IPEndPoint(IPAddress.Any, port));
+        }
+
+        public void Listen(int backlog)
+        {
+            _socket.Listen(500);
+        }
+
+        public void Accept()
+        {
+            _socket.BeginAccept(AcceptedCallback, null);
+            Accept();
+        }
+
+        private void AcceptedCallback(IAsyncResult result)
+        {
+            Socket clientSocket = _socket.EndAccept(result);
+            _buffer = new byte[1024];
+            clientSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, RecievedCallback, clientSocket);
+            Accept();
+        }
+
+        private void RecievedCallback(IAsyncResult result)
+        {
+            Socket clientSocket = result.AsyncState as Socket;
+            int bufferSize = clientSocket.EndReceive(result);
+            byte[] packet = new byte[bufferSize];
+            Array.Copy(_buffer, packet, packet.Length);
+
+            // Handle packet.
+            _buffer = new byte[1024];
+            clientSocket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, RecievedCallback, clientSocket);
+        }
+    }
+}
